30f3260ede579b295b96f5b958dc74dc47758a16
Yaroslav Tsalko <55207859+ImperatorYarik@users.noreply.github.com>
2025-07-23 20:06:48 +0300

    Basic JavaScript file

---
+++ script.js
@@ -0,0 +1,3 @@
+// Basic testimonial carousel functionality
+const testimonials = document.querySelector('.testimonial-carousel');
+// You can add more complex carousel logic here using JavaScript libraries
