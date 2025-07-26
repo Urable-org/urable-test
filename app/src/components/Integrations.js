690bcad71f3864d2bf7bececf5a377670bdaae1c
Yaroslav Tsalko <55207859+ImperatorYarik@users.noreply.github.com>
2025-07-26 16:01:53 +0300

    feat(integrations): add Integrations component

---
+++ app/src/components/Integrations.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Integrations() {
+  return (
+    <section className="integrations">
+      <h2>Integrations</h2>
+      <p>Urable seamlessly integrates with your favorite tools:</p>
+      <ul>
+        <li>Jira</li>
+        <li>GitHub</li>
+        <li>Slack</li>
+      </ul>
+    </section>
+  );
+}
+
+export default Integrations;
